b3ff1de04802eea710a83e5c43c59bc5968c9ecb
Himanshu4313 <hk225064@gmail.com>
2023-08-30 16:41:21 +0530

    change somefile name due to error

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <div className="github-container">
         <SearchUser UpdateSearchTerm={setsearchTerm} />
-         <GithubDetails key={searchTerm}  GithubUserName={searchTerm}/>
+         <GithubDetails  key={searchTerm}  GithubUserName={searchTerm}/>
       </div>
     </>
   );
