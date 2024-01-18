fdfc13f797949c473d26d7ddf655da4708b210b5
Himanshu4313 <hk225064@gmail.com>
2024-01-18 20:08:57 +0530

    path resolve

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import SearchUser from "./Components/SearchUser/SearchUser";
-import GithubDetails from "./Components/githubDetails/githubDetails";
+import GithubDetails from "./Components/githubDetails/githubDetails.jsx";
 
 function App() {
   const [searchTerm, setsearchTerm] = useState("");
@@ -12,6 +12,7 @@ function App() {
       <div className="github-container">
         <SearchUser UpdateSearchTerm={setsearchTerm} />
          <GithubDetails  key={searchTerm}  GithubUserName={searchTerm}/>
+         
       </div>
     </>
   );
